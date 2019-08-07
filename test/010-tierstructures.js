161e9e2b684c5312666825d4190c30ab0fd82084
Armand Smit <armand@agilite.io>
2019-08-07 08:17:09 +0200

    Updated Tier Structure Tests

--- test/010-tierstructures.js
+++ test/010-tierstructures.js
@@ -5,7 +5,7 @@ const UUID = require('uuid')
 const TypeDetect = require('type-detect')
 const expect = require('chai').expect
 const Agilite = require('../controllers/agilite')
-const Enums = require('../utils/enums')
+const EnumsTypeDetect = require('../utils/enums-type-detect')
 const DataTemplate = require('../data-templates/tierstructures')
 
 const agilite = new Agilite({
@@ -18,6 +18,135 @@ describe('Agilit-e Tier Structures', () => {
   let tmpEntry = null
   let recordId = null
   let key = UUID.v1()
+  let error = null
+
+  it('Create New Invalid Record - Empty Body Object', (done) => {
+    mainEntry = JSON.parse(JSON.stringify(DataTemplate.emptyObject))
+
+    agilite.TierStructures.postData(mainEntry)
+      .then((response) => {})
+      .catch((err) => {
+        error = err.response.data
+
+        expect(TypeDetect(error)).to.equal(EnumsTypeDetect.OBJECT)
+
+        // Check if statusCode exists and contains correct value
+        expect(error).to.haveOwnProperty('statusCode')
+        expect(error.statusCode).to.equal(400)
+
+        // Check if errorMessage exists and contains correct error message
+        expect(error).to.haveOwnProperty('errorMessage')
+        expect(error.errorMessage).to.equal("No 'data' property found in JSON Body")
+
+        // Check if additionalMessages exists and is blank array
+        expect(error).to.haveOwnProperty('additionalMessages')
+        expect(TypeDetect(error.additionalMessages)).to.equal(EnumsTypeDetect.ARRAY)
+      })
+      .then(done, done)
+  })
+
+  it('Create New Invalid Record - No Key', (done) => {
+    mainEntry = JSON.parse(JSON.stringify(DataTemplate.emptyDataObject))
+
+    agilite.TierStructures.postData(mainEntry)
+      .then((response) => {})
+      .catch((err) => {
+        error = err.response.data
+
+        expect(TypeDetect(error)).to.equal(EnumsTypeDetect.OBJECT)
+
+        // Check if statusCode exists and contains correct value
+        expect(error).to.haveOwnProperty('statusCode')
+        expect(error.statusCode).to.equal(400)
+
+        // Check if errorMessage exists and contains correct error message
+        expect(error).to.haveOwnProperty('errorMessage')
+        expect(error.errorMessage).to.equal("Please provide a valid Profile 'key'")
+
+        // Check if additionalMessages exists and is blank array
+        expect(error).to.haveOwnProperty('additionalMessages')
+        expect(TypeDetect(error.additionalMessages)).to.equal(EnumsTypeDetect.ARRAY)
+      })
+      .then(done, done)
+  })
+
+  it('Create New Invalid Record - Invalid Values Array Entry', (done) => {
+    mainEntry = JSON.parse(JSON.stringify(DataTemplate.invalidValues.invalidObject))
+    mainEntry.data.key = key
+
+    agilite.TierStructures.postData(mainEntry)
+      .then((response) => {})
+      .catch((err) => {
+        error = err.response.data
+
+        expect(TypeDetect(error)).to.equal(EnumsTypeDetect.OBJECT)
+
+        // Check if statusCode exists and contains correct value
+        expect(error).to.haveOwnProperty('statusCode')
+        expect(error.statusCode).to.equal(400)
+
+        // Check if errorMessage exists and contains correct error message
+        expect(error).to.haveOwnProperty('errorMessage')
+        expect(error.errorMessage).to.equal("Please provide a valid 'label' property in the 'values' entry")
+
+        // Check if additionalMessages exists and is blank array
+        expect(error).to.haveOwnProperty('additionalMessages')
+        expect(TypeDetect(error.additionalMessages)).to.equal(EnumsTypeDetect.ARRAY)
+      })
+      .then(done, done)
+  })
+
+  it('Create New Invalid Record - Invalid Label Property', (done) => {
+    mainEntry = JSON.parse(JSON.stringify(DataTemplate.invalidValues.invalidLabel))
+    mainEntry.data.key = key
+
+    agilite.TierStructures.postData(mainEntry)
+      .then((response) => {})
+      .catch((err) => {
+        error = err.response.data
+
+        expect(TypeDetect(error)).to.equal(EnumsTypeDetect.OBJECT)
+
+        // Check if statusCode exists and contains correct value
+        expect(error).to.haveOwnProperty('statusCode')
+        expect(error.statusCode).to.equal(400)
+
+        // Check if errorMessage exists and contains correct error message
+        expect(error).to.haveOwnProperty('errorMessage')
+        expect(error.errorMessage).to.equal("Please provide a valid 'label' property in the 'values' entry")
+
+        // Check if additionalMessages exists and is blank array
+        expect(error).to.haveOwnProperty('additionalMessages')
+        expect(TypeDetect(error.additionalMessages)).to.equal(EnumsTypeDetect.ARRAY)
+      })
+      .then(done, done)
+  })
+
+  it('Create New Invalid Record - Invalid Value Property', (done) => {
+    mainEntry = JSON.parse(JSON.stringify(DataTemplate.invalidValues.invalidValue))
+    mainEntry.data.key = key
+
+    agilite.TierStructures.postData(mainEntry)
+      .then((response) => {})
+      .catch((err) => {
+        error = err.response.data
+
+        expect(TypeDetect(error)).to.equal(EnumsTypeDetect.OBJECT)
+
+        // Check if statusCode exists and contains correct value
+        expect(error).to.haveOwnProperty('statusCode')
+        expect(error.statusCode).to.equal(400)
+
+        // Check if errorMessage exists and contains correct error message
+        expect(error).to.haveOwnProperty('errorMessage')
+        expect(error.errorMessage).to.equal("Please provide a valid 'value' property in the 'values' entry")
+
+        // Check if additionalMessages exists and is blank array
+        expect(error).to.haveOwnProperty('additionalMessages')
+        expect(TypeDetect(error.additionalMessages)).to.equal(EnumsTypeDetect.ARRAY)
+      })
+      .then(done, done)
+  })
 
   it('Create New Record', (done) => {
     mainEntry = JSON.parse(JSON.stringify(DataTemplate.new))
@@ -25,7 +154,7 @@ describe('Agilit-e Tier Structures', () => {
 
     agilite.TierStructures.postData(mainEntry)
       .then((response) => {
-        expect(TypeDetect(response.data)).to.equal(Enums.VALUE_OBJECT_PROPER)
+        expect(TypeDetect(response.data)).to.equal(EnumsTypeDetect.OBJECT)
 
         // Check if Optional Fields exists
         expect(response.data.data).to.haveOwnProperty('isActive')
@@ -51,7 +180,7 @@ describe('Agilit-e Tier Structures', () => {
 
     agilite.TierStructures.getData()
       .then((response) => {
-        expect(TypeDetect(response.data)).to.equal(Enums.VALUE_ARRAY_PROPER)
+        expect(TypeDetect(response.data)).to.equal(EnumsTypeDetect.ARRAY)
         expect(response.data.length).to.be.greaterThan(0)
 
         for (let x in response.data) {
@@ -72,6 +201,186 @@ describe('Agilit-e Tier Structures', () => {
       .then(done, done)
   })
 
+  it('Update Record - Empty Body Object', (done) => {
+    mainEntry = JSON.parse(JSON.stringify(DataTemplate.emptyObject))
+
+    agilite.TierStructures.putData(recordId, mainEntry)
+      .then((response) => {})
+      .catch((err) => {
+        error = err.response.data
+
+        expect(TypeDetect(error)).to.equal(EnumsTypeDetect.OBJECT)
+
+        // Check if statusCode exists and contains correct value
+        expect(error).to.haveOwnProperty('statusCode')
+        expect(error.statusCode).to.equal(400)
+
+        // Check if errorMessage exists and contains correct error message
+        expect(error).to.haveOwnProperty('errorMessage')
+        expect(error.errorMessage).to.equal("No 'data' property found in JSON Body")
+
+        // Check if additionalMessages exists and is blank array
+        expect(error).to.haveOwnProperty('additionalMessages')
+        expect(TypeDetect(error.additionalMessages)).to.equal(EnumsTypeDetect.ARRAY)
+      })
+      .then(done, done)
+  })
+
+  it('Update Record - No Key', (done) => {
+    mainEntry = JSON.parse(JSON.stringify(DataTemplate.emptyDataObject))
+
+    agilite.TierStructures.putData(recordId, mainEntry)
+      .then((response) => {})
+      .catch((err) => {
+        error = err.response.data
+
+        expect(TypeDetect(error)).to.equal(EnumsTypeDetect.OBJECT)
+
+        // Check if statusCode exists and contains correct value
+        expect(error).to.haveOwnProperty('statusCode')
+        expect(error.statusCode).to.equal(400)
+
+        // Check if errorMessage exists and contains correct error message
+        expect(error).to.haveOwnProperty('errorMessage')
+        expect(error.errorMessage).to.equal("Please provide a valid Profile 'key'")
+
+        // Check if additionalMessages exists and is blank array
+        expect(error).to.haveOwnProperty('additionalMessages')
+        expect(TypeDetect(error.additionalMessages)).to.equal(EnumsTypeDetect.ARRAY)
+      })
+      .then(done, done)
+  })
+
+  it('Update Record - Invalid Values Array Entry', (done) => {
+    mainEntry = JSON.parse(JSON.stringify(DataTemplate.invalidValues.invalidObject))
+    mainEntry.data.key = key
+
+    agilite.TierStructures.putData(recordId, mainEntry)
+      .then((response) => {})
+      .catch((err) => {
+        error = err.response.data
+
+        expect(TypeDetect(error)).to.equal(EnumsTypeDetect.OBJECT)
+
+        // Check if statusCode exists and contains correct value
+        expect(error).to.haveOwnProperty('statusCode')
+        expect(error.statusCode).to.equal(400)
+
+        // Check if errorMessage exists and contains correct error message
+        expect(error).to.haveOwnProperty('errorMessage')
+        expect(error.errorMessage).to.equal("Please provide a valid 'label' property in the 'values' entry")
+
+        // Check if additionalMessages exists and is blank array
+        expect(error).to.haveOwnProperty('additionalMessages')
+        expect(TypeDetect(error.additionalMessages)).to.equal(EnumsTypeDetect.ARRAY)
+      })
+      .then(done, done)
+  })
+
+  it('Update Record - Invalid Label Property', (done) => {
+    mainEntry = JSON.parse(JSON.stringify(DataTemplate.invalidValues.invalidLabel))
+    mainEntry.data.key = key
+
+    agilite.TierStructures.putData(recordId, mainEntry)
+      .then((response) => {})
+      .catch((err) => {
+        error = err.response.data
+
+        expect(TypeDetect(error)).to.equal(EnumsTypeDetect.OBJECT)
+
+        // Check if statusCode exists and contains correct value
+        expect(error).to.haveOwnProperty('statusCode')
+        expect(error.statusCode).to.equal(400)
+
+        // Check if errorMessage exists and contains correct error message
+        expect(error).to.haveOwnProperty('errorMessage')
+        expect(error.errorMessage).to.equal("Please provide a valid 'label' property in the 'values' entry")
+
+        // Check if additionalMessages exists and is blank array
+        expect(error).to.haveOwnProperty('additionalMessages')
+        expect(TypeDetect(error.additionalMessages)).to.equal(EnumsTypeDetect.ARRAY)
+      })
+      .then(done, done)
+  })
+
+  it('Update Record - Invalid Value Property', (done) => {
+    mainEntry = JSON.parse(JSON.stringify(DataTemplate.invalidValues.invalidValue))
+    mainEntry.data.key = key
+
+    agilite.TierStructures.putData(recordId, mainEntry)
+      .then((response) => {})
+      .catch((err) => {
+        error = err.response.data
+
+        expect(TypeDetect(error)).to.equal(EnumsTypeDetect.OBJECT)
+
+        // Check if statusCode exists and contains correct value
+        expect(error).to.haveOwnProperty('statusCode')
+        expect(error.statusCode).to.equal(400)
+
+        // Check if errorMessage exists and contains correct error message
+        expect(error).to.haveOwnProperty('errorMessage')
+        expect(error.errorMessage).to.equal("Please provide a valid 'value' property in the 'values' entry")
+
+        // Check if additionalMessages exists and is blank array
+        expect(error).to.haveOwnProperty('additionalMessages')
+        expect(TypeDetect(error.additionalMessages)).to.equal(EnumsTypeDetect.ARRAY)
+      })
+      .then(done, done)
+  })
+
+  it('Update Record - No Record ID', (done) => {
+    mainEntry = JSON.parse(JSON.stringify(DataTemplate.modified))
+    mainEntry.data.key = key
+
+    agilite.TierStructures.putData(undefined, mainEntry)
+      .then((response) => {})
+      .catch((err) => {
+        error = err.response.data
+
+        expect(TypeDetect(error)).to.equal(EnumsTypeDetect.OBJECT)
+
+        // Check if statusCode exists and contains correct value
+        expect(error).to.haveOwnProperty('statusCode')
+        expect(error.statusCode).to.equal(400)
+
+        // Check if errorMessage exists and contains correct error message
+        expect(error).to.haveOwnProperty('errorMessage')
+        expect(error.errorMessage).to.equal("No Id was specified in the 'record-id' header parameter")
+
+        // Check if additionalMessages exists and is blank array
+        expect(error).to.haveOwnProperty('additionalMessages')
+        expect(TypeDetect(error.additionalMessages)).to.equal(EnumsTypeDetect.ARRAY)
+      })
+      .then(done, done)
+  })
+
+  it('Update Record - Non-existant Record ID', (done) => {
+    mainEntry = JSON.parse(JSON.stringify(DataTemplate.modified))
+    mainEntry.data.key = key
+
+    agilite.TierStructures.putData("test", mainEntry)
+      .then((response) => {})
+      .catch((err) => {
+        error = err.response.data
+
+        expect(TypeDetect(error)).to.equal(EnumsTypeDetect.OBJECT)
+
+        // Check if statusCode exists and contains correct value
+        expect(error).to.haveOwnProperty('statusCode')
+        expect(error.statusCode).to.equal(400)
+
+        // Check if errorMessage exists and contains correct error message
+        expect(error).to.haveOwnProperty('errorMessage')
+        expect(error.errorMessage).to.equal("Record with id: 'test' cannot be found")
+
+        // Check if additionalMessages exists and is blank array
+        expect(error).to.haveOwnProperty('additionalMessages')
+        expect(TypeDetect(error.additionalMessages)).to.equal(EnumsTypeDetect.ARRAY)
+      })
+      .then(done, done)
+  })
+
   it('Update Record', (done) => {
     expect(recordId).to.not.equal(null)
     key = 'PUT_' + key
@@ -81,7 +390,7 @@ describe('Agilit-e Tier Structures', () => {
 
     agilite.TierStructures.putData(recordId, mainEntry)
       .then((response) => {
-        expect(TypeDetect(response.data)).to.equal(Enums.VALUE_OBJECT_PROPER)
+        expect(TypeDetect(response.data)).to.equal(EnumsTypeDetect.OBJECT)
 
         // Check if Tier Entries has more than 1 value
         expect(response.data.data.tierEntries.length).to.be.greaterThan(0)
@@ -92,10 +401,33 @@ describe('Agilit-e Tier Structures', () => {
       .then(done, done)
   })
 
+  it('Get Tier By Key - No Headers', (done) => {
+    agilite.TierStructures.getTierByKey()
+      .then((response) => {})
+      .catch((err) => {
+        error = err.response.data
+
+        expect(TypeDetect(error)).to.equal(EnumsTypeDetect.OBJECT)
+
+        // Check if statusCode exists and contains correct value
+        expect(error).to.haveOwnProperty('statusCode')
+        expect(error.statusCode).to.equal(400)
+
+        // Check if errorMessage exists and contains correct error message
+        expect(error).to.haveOwnProperty('errorMessage')
+        expect(error.errorMessage).to.equal("No Tier Keys were specified in the 'tier-keys' header parameter")
+
+        // Check if additionalMessages exists and is blank array
+        expect(error).to.haveOwnProperty('additionalMessages')
+        expect(TypeDetect(error.additionalMessages)).to.equal(EnumsTypeDetect.ARRAY)
+      })
+      .then(done, done)
+  })
+
   it('Get Tier By Key - Tier 1', (done) => {
     agilite.TierStructures.getTierByKey([key], true, false, false, agilite.TierStructures.sort.ASC, agilite.TierStructures.outputFormat.ARRAY)
       .then((response) => {
-        expect(TypeDetect(response.data)).to.equal(Enums.VALUE_ARRAY_PROPER)
+        expect(TypeDetect(response.data)).to.equal(EnumsTypeDetect.ARRAY)
         expect(response.data.length).to.be.greaterThan(0)
       })
       //  .catch((err) => {
@@ -104,12 +436,58 @@ describe('Agilit-e Tier Structures', () => {
       .then(done, done)
   })
 
+  it('Delete Record - No Record ID', (done) => {
+    agilite.TierStructures.deleteData()
+      .then((response) => {})
+      .catch((err) => {
+        error = err.response.data
+
+        expect(TypeDetect(error)).to.equal(EnumsTypeDetect.OBJECT)
+
+        // Check if statusCode exists and contains correct value
+        expect(error).to.haveOwnProperty('statusCode')
+        expect(error.statusCode).to.equal(400)
+
+        // Check if errorMessage exists and contains correct error message
+        expect(error).to.haveOwnProperty('errorMessage')
+        expect(error.errorMessage).to.equal("No Id was specified in the 'record-id' header parameter")
+
+        // Check if additionalMessages exists and is blank array
+        expect(error).to.haveOwnProperty('additionalMessages')
+        expect(TypeDetect(error.additionalMessages)).to.equal(EnumsTypeDetect.ARRAY)
+      })
+      .then(done, done)
+  })
+
+  it('Delete Record - Non-existant Record ID', (done) => {
+    agilite.TierStructures.deleteData("test")
+      .then((response) => {})
+      .catch((err) => {
+        error = err.response.data
+
+        expect(TypeDetect(error)).to.equal(EnumsTypeDetect.OBJECT)
+
+        // Check if statusCode exists and contains correct value
+        expect(error).to.haveOwnProperty('statusCode')
+        expect(error.statusCode).to.equal(400)
+
+        // Check if errorMessage exists and contains correct error message
+        expect(error).to.haveOwnProperty('errorMessage')
+        expect(error.errorMessage).to.equal("Record with id: 'test' cannot be found")
+
+        // Check if additionalMessages exists and is blank array
+        expect(error).to.haveOwnProperty('additionalMessages')
+        expect(TypeDetect(error.additionalMessages)).to.equal(EnumsTypeDetect.ARRAY)
+      })
+      .then(done, done)
+  })
+
   it('Delete Record', (done) => {
     expect(recordId).to.not.equal(null)
 
     agilite.TierStructures.deleteData(recordId)
       .then((response) => {
-        expect(TypeDetect(response.data)).to.equal(Enums.VALUE_OBJECT_PROPER)
+        expect(TypeDetect(response.data)).to.equal(EnumsTypeDetect.OBJECT)
         expect(JSON.stringify(response.data)).to.equal('{}')
       })
       //  .catch((err) => {
